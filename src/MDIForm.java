eedd3e6348ab7231d5a99f0449a8b92461335823
ghanshambordekar3 <ghanshamzzx@gmail.com>
2025-08-06 22:28:06 +0530

    I Changed login, and add GlobalClass,DBConnection,MDIForm File

---
+++ src/MDIForm.java
@@ -0,0 +1,101 @@
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+
+public class MDIForm extends JFrame implements ActionListener
+{
+	JMenuBar mBar;
+	
+	JMenu mnuMaster,mnuView,mnuTools;
+	
+	JMenuItem mnuInWord,mnuOutWord,mnuApplicationStatus,mnuBackup;
+	
+	JDesktopPane Desktop;
+	
+	JLabel lbName,lbText;
+	
+	MDIForm()
+	{
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+	        // Set main layout
+	    getContentPane().setLayout(new BorderLayout());
+		
+		setTitle("In Word - Out Word");
+			
+		mBar = new JMenuBar();
+		setJMenuBar(mBar);
+		
+//		setLayout(null);
+		
+		mnuMaster = new JMenu("Master");
+		mBar.add(mnuMaster);
+		mnuMaster.setMnemonic(KeyEvent.VK_M);				//Shortcut key i.e, Alt + M;
+		
+		mnuInWord = new JMenuItem("In-Word");
+		mnuMaster.add(mnuInWord);
+		mnuInWord.addActionListener(this);
+		
+		mnuOutWord = new JMenuItem("Out-Word");
+		mnuMaster.add(mnuOutWord);
+		mnuOutWord.addActionListener(this);
+		
+		mnuView = new JMenu("View");
+		mBar.add(mnuView);
+		mnuView.setMnemonic(KeyEvent.VK_V);							//Shortcut key i.e, Alt + M;
+		
+		mnuApplicationStatus = new JMenuItem("Application Status");
+		mnuView.add(mnuApplicationStatus);
+		mnuApplicationStatus.addActionListener(this);
+		
+		mnuTools = new JMenu("Tools");
+		mBar.add(mnuTools);
+		mnuTools.setMnemonic(KeyEvent.VK_T);				//Shortcut key i.e, Alt + M;
+		
+		mnuBackup = new JMenuItem("Backup");
+		mnuTools.add(mnuBackup);
+		mnuBackup.addActionListener(this);
+		
+	       // Top panel with greeting
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        lbText = new JLabel("Hello " + GlobalClass.uName);
+        topPanel.add(lbText);
+        getContentPane().add(topPanel, BorderLayout.NORTH);
+
+        // Center desktop pane
+        Desktop = new JDesktopPane();
+        getContentPane().add(Desktop, BorderLayout.CENTER);
+
+        // Footer panel
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.X_AXIS));
+        bottomPanel.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+
+        lbName = new JLabel("© 2025 Ghansham Bordekar. All Rights Reserved | Contact: 8849826386 / ghanshambordekar@gmail.com");
+        bottomPanel.add(Box.createHorizontalGlue());
+        bottomPanel.add(lbName);
+        bottomPanel.add(Box.createHorizontalGlue());
+
+        getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+
+        setVisible(true);
+        
+     // Center desktop pane wrapped in JScrollPane
+        
+		//Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	    //setSize(screenSize.width, screenSize.height);
+
+        Desktop = new JDesktopPane();
+       // Desktop.setPreferredSize(new Dimension(screenSize.width, screenSize.height));
+        JScrollPane scrollPane = new JScrollPane(Desktop,
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        getContentPane().add(scrollPane, BorderLayout.CENTER);
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+
+	}
+}
--- src/login.java
+++ src/login.java
@@ -1,200 +1,234 @@
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.event.*;
 import java.awt.event.*;
 import java.io.*;
 
 public class login extends JFrame implements ActionListener 
 {
+	JPanel loginPanel,RestorePanel;
+	
+	JLabel lbUserName,lbPassword,lbLine,lbRestore,lbRestoreFile;
+	
+	JTextField txUserName,txRestoreFile;
+	
+	JPasswordField txPassword;
+	
+	JButton btnLogin,btnCancel,btnRestore,btnBrowse,btnrestore;
+	
+	private String selectedFilePath = null;
+	
+	final int restorePanelFullWidth = 540;
+	final int restorePanelHeight = 200;
+	
+	login()
+	{
+		GlobalClass.setConnection();
+		
+		setSize(600,400);
+		setLocation(400,0);
+		setTitle("Login");
+		
+		setLayout(null);
+		
+		loginPanel = new JPanel();
+		loginPanel.setBounds(20,20,540,200);
+		loginPanel.setBorder(BorderFactory.createTitledBorder("Login"));
+		add(loginPanel);
+		
+		loginPanel.setLayout(null);
+		
+		lbUserName = new JLabel("UserName :");
+		lbUserName.setBounds(130,50,100,20);
+		loginPanel.add(lbUserName);
+		
+		txUserName = new JTextField();
+		txUserName.setBounds(230,50,150,20);
+		loginPanel.add(txUserName);
+		
+		lbPassword = new JLabel("Password :");
+		lbPassword.setBounds(130,100,100,20);
+		loginPanel.add(lbPassword);
+		
+		txPassword = new JPasswordField();
+		txPassword.setBounds(230,100,150,20);
+		loginPanel.add(txPassword);
+		
+		btnLogin = new JButton("Login");
+		btnLogin.setBounds(150,240,100,20);
+		add(btnLogin);
+		btnLogin.addActionListener(this);
+		
+		btnCancel = new JButton("Cancel");
+		btnCancel.setBounds(300,240,100,20);
+		add(btnCancel);
+		btnCancel.addActionListener(this);
+		
+		lbLine = new JLabel("_______________________________________________________________________________________");
+		lbLine.setBounds(20,260,540,20);
+		add(lbLine);
+		
+		lbRestore = new JLabel("Restore Backup Data : ");
+		lbRestore.setBounds(220,300,150,20);
+		add(lbRestore);
+		
+		btnRestore = new JButton("Restore");
+		btnRestore.setBounds(230,330,100,20);
+		add(btnRestore);
+		btnRestore.addActionListener(this);
+		
+		RestorePanel = new JPanel();
+		RestorePanel.setBounds(20,420,540,200);
+		RestorePanel.setBorder(BorderFactory.createTitledBorder("Restore"));
+		add(RestorePanel);
+		RestorePanel.setVisible(false);
+		
+		RestorePanel.setLayout(null);
+		
+		lbRestoreFile = new JLabel("Choose Restore File :");
+		lbRestoreFile.setBounds(20,50,150,20);
+		RestorePanel.add(lbRestoreFile);
+		
+		txRestoreFile = new JTextField();
+		txRestoreFile.setBounds(180,50,150,20);
+		RestorePanel.add(txRestoreFile);
+		
+		btnBrowse = new JButton("Browse");
+		btnBrowse.setBounds(360,50,100,20);
+		RestorePanel.add(btnBrowse);
+		btnBrowse.addActionListener(this);
+		
+		btnrestore = new JButton("Restore");
+		btnrestore.setBounds(205,130,100,20);
+		RestorePanel.add(btnrestore);
+		btnrestore.addActionListener(this);
+	}
+	
+	 private void animateRestorePanelWipeIn() {
+	        RestorePanel.setVisible(true);
+	        final int startX = 20;
+	        final int y = 420;
 
-    JPanel loginPanel, restorePanel;
-    JLabel lbUserName, lbPassword, lbRestoreFolder, lbLine, lbRestoreData;
-    JTextField txUserName, txFolder;
-    JPasswordField txPassword;
-    JButton btnLogin, btnCancel, btnRestoreUI, btnBrowse, btnRestore;
-    private String selectedFilePath = null;
+	        Timer timer = new Timer(5, null);
+	        timer.addActionListener(new ActionListener() {
+	            int w = 0;
+	            public void actionPerformed(ActionEvent e) {
+	                if (w < restorePanelFullWidth) {
+	                    w += 10;
+	                    RestorePanel.setBounds(startX, y, w, restorePanelHeight);
+	                    RestorePanel.repaint();
+	                } else {
+	                    RestorePanel.setBounds(startX, y, restorePanelFullWidth, restorePanelHeight);
+	                    timer.stop();
+	                }
+	            }
+	        });
+	        timer.start();
+	    }
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource() == btnRestore)
+		{
+			setSize(600,670);
+			RestorePanel.setVisible(true);
+			animateRestorePanelWipeIn();			
+		}
+		if(e.getSource() == btnLogin)
+		{
+			if(txUserName.getText().toString().equals("admin"))
+			{
+				if(txPassword.getText().equals("1234"))
+				{
+					GlobalClass.uName = txUserName.getText();
+					MDIForm obj = new MDIForm();
+					obj.show();
+					this.hide();
+				}
+				else
+				{
+					txPassword.setText("");
+					txPassword.requestFocus();
+				}
+			}
+			else
+			{
+				txUserName.setText("");
+				txPassword.setText("");
+				txUserName.requestFocus();
+				JOptionPane.showMessageDialog(null,"Invalid State");
+			}
+		}
+		
+		if(e.getSource() == btnCancel)
+		{
+			System.exit(0);
+		}
+		
+		if(e.getSource() == btnBrowse)
+		{
+			JFileChooser fileChooser = new JFileChooser();
+		    fileChooser.setAcceptAllFileFilterUsed(false);
+		    fileChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("SQL Files", "sql"));			
+			int result = fileChooser.showOpenDialog(this);
+			
+			if(result == JFileChooser.APPROVE_OPTION)
+			{
+				File selected = fileChooser.getSelectedFile();
+				selectedFilePath = selected.getAbsolutePath();
+				txRestoreFile.setText(selectedFilePath);
+			}
+		}
 
-    public login() 
-    {
-        setTitle("Login Page");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(650, 500);
-        setLocation(400, 0);
-        setLocationRelativeTo(null);
-
-        // Container Panel with GridBagLayout
-        JPanel container = new JPanel(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-
-        // Login Info Panel
-        loginPanel = new JPanel(null);
-        loginPanel.setPreferredSize(new Dimension(530, 200));
-        loginPanel.setBorder(BorderFactory.createTitledBorder("Login State"));
-
-        lbUserName = new JLabel("UserName");
-        lbUserName.setBounds(150, 50, 100, 20);
-        loginPanel.add(lbUserName);
-
-        txUserName = new JTextField();
-        txUserName.setBounds(250, 50, 100, 20);
-        loginPanel.add(txUserName);
-
-        lbPassword = new JLabel("Password");
-        lbPassword.setBounds(150, 100, 100, 20);
-        loginPanel.add(lbPassword);
-
-        txPassword = new JPasswordField();
-        txPassword.setBounds(250, 100, 100, 20);
-        loginPanel.add(txPassword);
-
-        // Add login panel to container
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        container.add(loginPanel, gbc);
-
-        // Buttons Panel
-        JPanel btnPanel = new JPanel(null);
-        btnPanel.setPreferredSize(new Dimension(530, 80));
-
-        btnLogin = new JButton("Login");
-        btnLogin.setBounds(125, 10, 100, 25);
-        btnLogin.addActionListener(this);
-        btnPanel.add(btnLogin);
-
-        btnCancel = new JButton("Cancel");
-        btnCancel.setBounds(275, 10, 100, 25);
-        btnCancel.addActionListener(this);
-        btnPanel.add(btnCancel);
-
-        gbc.gridy++;
-        container.add(btnPanel, gbc);
-
-        // Line Label
-        lbLine = new JLabel("________________________________________________________________________________________");
-        gbc.gridy++;
-        container.add(lbLine, gbc);
-
-        // Restore Label + Button
-        JPanel restoreTrigger = new JPanel(null);
-        restoreTrigger.setPreferredSize(new Dimension(530, 60));
-
-        lbRestoreData = new JLabel("Restore Backup Data:");
-        lbRestoreData.setBounds(190, 5, 200, 20);
-        restoreTrigger.add(lbRestoreData);
-
-        btnRestoreUI = new JButton("Restore");
-        btnRestoreUI.setBounds(200, 30, 100, 25);
-        btnRestoreUI.addActionListener(this);
-        restoreTrigger.add(btnRestoreUI);
-
-        gbc.gridy++;
-        container.add(restoreTrigger, gbc);
-
-        // Restore Panel
-        restorePanel = new JPanel(null);
-        restorePanel.setPreferredSize(new Dimension(530, 180));
-        restorePanel.setBorder(BorderFactory.createTitledBorder("Restore Data"));
-        restorePanel.setVisible(false);
-
-        lbRestoreFolder = new JLabel("Choose Restore File:");
-        lbRestoreFolder.setBounds(40, 40, 150, 20);
-        restorePanel.add(lbRestoreFolder);
-
-        txFolder = new JTextField();
-        txFolder.setBounds(190, 40, 160, 20);
-        txFolder.setEditable(false);
-        restorePanel.add(txFolder);
-
-        btnBrowse = new JButton("Browse");
-        btnBrowse.setBounds(370, 40, 100, 20);
-        btnBrowse.addActionListener(this);
-        restorePanel.add(btnBrowse);
-
-        btnRestore = new JButton("Restore Now");
-        btnRestore.setBounds(210, 100, 120, 25);
-        btnRestore.addActionListener(this);
-        restorePanel.add(btnRestore);
-
-        gbc.gridy++;
-        container.add(restorePanel, gbc);
-
-        // Wrap the container in a scroll pane
-        JScrollPane scroll = new JScrollPane(container,
-                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        setContentPane(scroll);
-        setVisible(true);
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == btnLogin) {
-            String u = txUserName.getText().trim();
-            String p = new String(txPassword.getPassword());
-            if (u.equals("admin") && p.equals("1234")) {
-
-                dispose();
-            } else {
-                JOptionPane.showMessageDialog(this, "Invalid login");
-                txUserName.setText("");
-                txPassword.setText("");
-                txUserName.requestFocus();
-            }
-        }
-
-        if (e.getSource() == btnCancel) {
-            System.exit(0);
-        }
-
-        if (e.getSource() == btnRestoreUI) {
-            restorePanel.setVisible(true);
-            pack(); // resize frame to fit new content
-        }
-
-        if (e.getSource() == btnBrowse) {
-            JFileChooser fileChooser = new JFileChooser();
-            fileChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("SQL Files", "sql"));
-            if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-                File selected = fileChooser.getSelectedFile();
-                selectedFilePath = selected.getAbsolutePath();
-                txFolder.setText(selectedFilePath);
-            }
-        }
-
-        if (e.getSource() == btnRestore) {
-            restore();
-        }
-    }
-
-    public void restore() {
-        if (selectedFilePath == null || selectedFilePath.isEmpty()) {
+		
+		if(e.getSource() == btnrestore)
+		{
+			restore();
+		}
+	}
+	
+	public void restore()
+	{	
+		if (selectedFilePath == null || selectedFilePath.isEmpty()) 
+		{
             JOptionPane.showMessageDialog(this, "Please select a backup file first.");
             return;
         }
+		
+		String url = "InWordOutWord";
+		String user = "root";
+		String password = "admin";
+		String mysqlPath = "C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysql.exe";
+		
+		try
+		{		
+		     ProcessBuilder pb = new ProcessBuilder(
+		             mysqlPath,
+		             "-u", user,
+		             "-p" + password,
+		             url
+		    );
 
-        String db = "ShradaSchool";
-        String user = "root";
-        String pwd = "admin";
-        String mysql = "C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysql.exe";
+		    pb.redirectInput(new File(selectedFilePath)); 
+		    pb.redirectErrorStream(true);
+		    Process process = pb.start();
 
-        try {
+		    int status = process.waitFor();
 
-            ProcessBuilder pb = new ProcessBuilder(mysql, "-u" + user, "-p" + pwd, db);
-            pb.redirectInput(new File(selectedFilePath));
-            pb.redirectErrorStream(true);
+		    if (status == 0) 
+		    {
+		    	JOptionPane.showMessageDialog(this, "Restore completed successfully!");
+		    }
+		    else 
+		    {
+		        JOptionPane.showMessageDialog(this, "Restore failed with exit code: " + status);
+		    }		
+		 } 
+		catch (Exception ex) 
+		{
+			ex.printStackTrace();
+	        JOptionPane.showMessageDialog(this, "Error: " + ex.getMessage());
+	    }	
+	}
 
-            Process process = pb.start();
-            int result = process.waitFor();
-            if (result == 0)
-                JOptionPane.showMessageDialog(this, "Restore completed successfully!");
-            else
-                JOptionPane.showMessageDialog(this, "Restore failed with exit code: " + result);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(this, "Error: " + ex.getMessage());
-        }
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(login::new);
-    }
 }
-
